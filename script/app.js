2c14b9bfd2d52ce8d1f2f9f57c886525fe55e007
fahimuntasin <tasinfahimun@gmail.com>
2026-03-09 23:08:09 +0600

    mostly complete

--- script/app.js
+++ script/app.js
@@ -1,43 +1,47 @@
-
 const API = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
 
 let issues = []
 
 
-
-
+// LOGIN
 function login(){
 
 const user = document.getElementById("username").value
 const pass = document.getElementById("password").value
 
 if(user === "admin" && pass === "admin123"){
-
 window.location.href = "app.html"
-
 }else{
-
 alert("Invalid Credentials")
-
 }
 
 }
 
 
+// LOAD ALL ISSUES
 async function loadIssues(){
 
 showLoading()
 
-const res = await fetch(API)
+try{
 
+const res = await fetch(API)
 const data = await res.json()
 
 issues = data.data
 
 displayIssues(issues)
 
+}catch(error){
+
+console.error("Error loading issues", error)
+
 }
 
+}
+
+
+// DISPLAY ISSUE CARDS
 function displayIssues(list){
 
 const container = document.getElementById("issuesContainer")
@@ -48,7 +52,6 @@ document.getElementById("issueCount").innerText =
 list.length + " Issues"
 
 
-
 list.forEach(issue => {
 
 const border =
@@ -57,6 +60,17 @@ issue.status === "open"
 : "border-t-4 border-purple-500"
 
 
+const priority = (issue.priority || "").toLowerCase()
+
+let priorityColor = "badge-info"
+
+if(priority === "high"){
+priorityColor = "badge-error"
+}
+else if(priority === "medium"){
+priorityColor = "badge-warning"
+}
+
 
 const card = document.createElement("div")
 
@@ -64,12 +78,13 @@ card.className =
 `card bg-base-100 shadow ${border} cursor-pointer`
 
 
-
 card.innerHTML = `
 
 <div class="card-body">
 
-<h2 class="font-semibold">${issue.title}</h2>
+<h2 class="font-semibold">
+${issue.title}
+</h2>
 
 <p class="text-sm text-gray-500 line-clamp-2">
 ${issue.description}
@@ -79,16 +94,76 @@ ${issue.description}
 
 <span>${issue.author}</span>
 
-<span>${issue.priority}</span>
+<span class="badge ${priorityColor}">
+${issue.priority}
+</span>
 
 </div>
 
 </div>
+
 `
-card.onclick = () => openModal(issue)
+
+card.onclick = () => openModal(issue.id)
 
 container.appendChild(card)
 
 })
+
+}
+
+
+
+// FILTER ISSUES
+function filterIssues(type){
+
+document
+.querySelectorAll(".tab")
+.forEach(t => t.classList.remove("tab-active"))
+
+document
+.getElementById("tab-" + type)
+.classList.add("tab-active")
+
+
+if(type === "all"){
+
+displayIssues(issues)
+
+}
+else{
+
+const filtered =
+issues.filter(i => i.status === type)
+
+displayIssues(filtered)
+
+}
+
+}
+
+
+
+// SEARCH ISSUE
+async function searchIssue(){
+
+const text =
+document.getElementById("searchInput").value
+
+try{
+
+const res = await fetch(
+`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${text}`
+)
+
+const data = await res.json()
+
+displayIssues(data.data)
+
+}catch(error){
+
+console.error("Search error", error)
+
+}
 
 }
