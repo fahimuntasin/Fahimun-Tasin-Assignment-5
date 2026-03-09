68734c3a0a363348cb0a8f28ee38c4d3a1b36a30
fahimuntasin <tasinfahimun@gmail.com>
2026-03-09 20:52:50 +0600

    half done

--- script/app.js
+++ script/app.js
@@ -37,3 +37,58 @@ issues = data.data
 displayIssues(issues)
 
 }
+
+function displayIssues(list){
+
+const container = document.getElementById("issuesContainer")
+
+container.innerHTML = ""
+
+document.getElementById("issueCount").innerText =
+list.length + " Issues"
+
+
+
+list.forEach(issue => {
+
+const border =
+issue.status === "open"
+? "border-t-4 border-green-500"
+: "border-t-4 border-purple-500"
+
+
+
+const card = document.createElement("div")
+
+card.className =
+`card bg-base-100 shadow ${border} cursor-pointer`
+
+
+
+card.innerHTML = `
+
+<div class="card-body">
+
+<h2 class="font-semibold">${issue.title}</h2>
+
+<p class="text-sm text-gray-500 line-clamp-2">
+${issue.description}
+</p>
+
+<div class="flex justify-between text-xs mt-2">
+
+<span>${issue.author}</span>
+
+<span>${issue.priority}</span>
+
+</div>
+
+</div>
+`
+card.onclick = () => openModal(issue)
+
+container.appendChild(card)
+
+})
+
+}
